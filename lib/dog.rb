c6bb973ba7feee7bf312ac730423da4343a0237c
olusola olagoroye <solaolagoroye@gmail.com>
2019-12-18 02:48:22 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,31 @@
 # Add your code here
+class Dog 
+  @@all = []
+ def initialize(name)
+   @name = name
+
+   save
+end
+  def self.all
+    @@all
+  end
+  
+  def name
+    @name
+  end
+  
+  def self.print_all
+  self.all.each {|dog| puts dog.name}
+  
+  end
+  
+  def save
+    @@all << self
+    end
+  
+  def self.clear_all
+    @@all.clear
+  end
+end
+
+ # @@all.each {|dog| print_all[dog] += 1}
